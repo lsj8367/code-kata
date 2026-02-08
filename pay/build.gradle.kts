7dc94713cc1668f43b47f7a52e572b33311689a7
lsj8367 <lsj8367@naver.com>
2026-02-08 15:42:32 +0900

    feat: initial commit

---
+++ pay/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    id("org.springframework.boot") version "3.5.10"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.github.lsj8367"
+version = "0.0.1-SNAPSHOT"
+description = "pay"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.github.gavlyukovskiy:datasource-proxy-spring-boot-starter:1.9.2")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
